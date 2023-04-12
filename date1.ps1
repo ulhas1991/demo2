58d50f6a64b3185aec6a99f1226ace533cdafbd7
ulhas1991 <ulhas.us@gmail.com>
2023-04-12 08:58:32 +0530

    added

---
+++ date1.ps1
@@ -0,0 +1,6 @@
+﻿
+ $currentDate = Get-Date
+$lastMonthEndDate = $currentDate.AddDays(-($currentDate.Day))
+$lastMonthStartDate = $lastMonthEndDate.AddMonths(-1).AddDays(1)
+Write-Host $lastMonthStartDate.Date
+write-host $lastMonthEndDate.Date
